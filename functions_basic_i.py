9287ff5dc5220d799c21ef3f295c71e198741fc8
Jacob Johnson <johnsondljl7@yahoo.com>
2021-07-03 23:55:04 -0500

    Final/functions_basic_i

---
+++ functions_basic_i.py
@@ -0,0 +1,139 @@
+# #1
+# def number_of_food_groups():
+#     return 5
+# print(number_of_food_groups())
+# # prints "5" in the console 
+
+
+#2
+# def number_of_military_branches():
+#     return 5
+# print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
+# returns error 
+
+#3
+# def number_of_books_on_hold():
+#     return 5
+#     return 10
+# print(number_of_books_on_hold())
+# prints "5" in the console
+
+
+#4
+# def number_of_fingers():
+#     return 5
+#     print(10)
+# print(number_of_fingers())
+# prints 5
+
+#5
+# def number_of_great_lakes():
+#     print(5)
+# x = number_of_great_lakes()
+# print(x)
+# prints "5" in the console but x is undefined 
+
+#6
+# def add(b,c):
+#     print(b+c)
+# print(add(1,2) + add(2,3))
+# prints out 3 and then 5 but does not print both added together. no return statement
+
+#7
+# def concatenate(b,c):
+#     return str(b)+str(c)
+# print(concatenate(2,5))
+# prints out "25"
+
+
+#8
+# def number_of_oceans_or_fingers_or_continents():
+#     b = 100
+#     print(b)
+#     if b < 10:
+#         return 5
+#     else:
+#         return 10
+#     return 7
+# print(number_of_oceans_or_fingers_or_continents())
+# prints 100 and returns 10 and then prints it to the console
+
+#9
+# def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
+#     if b<c:
+#         return 7
+#     else:
+#         return 14
+#     return 3
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
+# prints out 7 then 14 then 21
+
+#10
+# def addition(b,c):
+#     return b+c
+#     return 10
+# print(addition(3,5))
+# returns and prints 8
+
+
+#11
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+# print(b)
+# foobar()
+# print(b)
+# prints 500 then 500 then 300 then 500
+
+
+#12
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# foobar()
+# print(b)
+# prints 500 then 500 then 300 then 500
+
+
+#13
+# b = 500
+# print(b)
+# def foobar():
+#     b = 300
+#     print(b)
+#     return b
+# print(b)
+# b=foobar()
+# print(b)
+# prints 500 then 500 then 300 then 300
+
+#14
+# def foo():
+#     print(1)
+#     bar()
+#     print(2)
+# def bar():
+#     print(3)
+# foo()
+# prints 1 then 3 then 2 
+
+#15
+# def foo():
+#     print(1)
+#     x = bar()
+#     print(x)
+#     return 10
+# def bar():
+#     print(3)
+#     return 5
+# y = foo()
+# print(y)
+# prints 1 then 3 then 5 then 10
